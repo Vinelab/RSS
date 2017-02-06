9375ad59468b03c18c3e4f8431f9da03336c5aaa
Gertt <gert@gert.gert>
2017-02-06 12:35:54 +0100

    [article] add support for retrieving the original XML element

--- src/Vinelab/Rss/Article.php
+++ src/Vinelab/Rss/Article.php
@@ -5,6 +5,13 @@ namespace Vinelab\Rss;
 class Article
 {
     /**
+     * Holds the original SimpleXMLElement
+     *
+     * @var SimpleXMLElement
+     */
+    protected $element = null;
+
+    /**
      * Holds the article information.
      *
      * @var array
@@ -18,6 +25,8 @@ class Article
      */
     public function __construct($article)
     {
+        $this->element = $article;
+
         foreach ($article as $attribute => $value) {
             // for enclosure, get the attributes as array
             if ($attribute == 'enclosure')
@@ -39,6 +48,16 @@ class Article
     public static function make($article)
     {
         return new static($article);
+    }
+
+    /**
+     * Get the original SimpleXMLElement
+     *
+     * @return SimpleXMLElement
+     */
+    public function getElement()
+    {
+        return $this->element;
     }
 
     /**
